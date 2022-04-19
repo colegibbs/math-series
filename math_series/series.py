85bcc649d728876ebf101d76ea5daa9ed0ebf889
Cole Gibbs <colegibbs0@gmail.com>
2022-04-19 15:05:46 -0700

    lucas function passing all tests

--- math_series/series.py
+++ math_series/series.py
@@ -4,4 +4,12 @@ def fibonacci(n):
   elif n == 1 or n == 2:
     return 1
   else: 
-    return fibonacci(n-1) + fibonacci(n-2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+def lucas(n):
+  if n == 0:
+    return 2
+  elif n == 1:
+    return 1
+  else:
+    return lucas(n - 1) + lucas(n - 2)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,4 @@
-from math_series.series import fibonacci
+from math_series.series import fibonacci, lucas
 
 def test_fibonacci_n_one():
   actual = fibonacci(1)
@@ -19,3 +19,24 @@ def test_fibonacci_n_twelve():
   actual = fibonacci(12)
   expected = 144
   assert actual == expected
+
+def test_lucas_n_zero():
+  actual = lucas(0)
+  expected = 2
+  assert actual == expected
+
+def test_lucas_n_one():
+  actual = lucas(1)
+  expected = 1
+  assert actual == expected
+
+def test_lucas_n_two():
+  actual = lucas(2)
+  expected = 3
+  assert actual == expected
+
+def test_lucas_n_ten():
+  actual = lucas(10)
+  expected = 123
+  assert actual == expected
+  
