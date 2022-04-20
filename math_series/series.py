574ba30de967e9857c73f564d841ee6e262c10fb
Cole Gibbs <colegibbs0@gmail.com>
2022-04-20 15:51:48 -0700

    docstrings

--- math_series/series.py
+++ math_series/series.py
@@ -1,4 +1,8 @@
 def fibonacci(n):
+  '''
+    This function executes the fibonacci sequence based on the parameter n. n is evaluated against the base cases of the fibonacci sequence, if neither base case is true, the fibonacci sequence is run based on n.
+  '''
+  
   if n == 0:
     return 0
   elif n == 1 or n == 2:
@@ -7,6 +11,9 @@ def fibonacci(n):
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 def lucas(n):
+  '''
+  This function executes the lucas sequence based on the parameter n. n is evaluated against the base cases of the lucas sequence, if neither base case is true, the lucas sequence is run based on n. 
+  '''
   if n == 0:
     return 2
   elif n == 1:
@@ -15,6 +22,9 @@ def lucas(n):
     return lucas(n - 1) + lucas(n - 2)
 
 def sum_series(n, nth_zero = 0, nth_one = 1):
+  '''
+  This function executes a fibonnaci/lucas style formula on the given parameters. The first parameter is the n that you wish the formula to execute on. The second is an optional base case of nth = 0. The third is an optional base case of nth = 1. If the last two arguments are ecluded, this function will execute as a fibbonaci sequence based on n. 
+  '''
   if n == 0:
     return nth_zero
   elif n == 1:
